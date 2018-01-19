9022b5fa9115b52b4cad950e9dff99a9a6fde76f
Vlad Lopalo <v.lopalo@modnakasta.ua>
2018-01-19 16:05:09 +0200

    update lein profile

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,15 @@
-{:user {:plugins [[venantius/ultra "0.5.1"]
-                  [lein-cljfmt "0.5.6"]
-                  [jonase/eastwood "0.2.3"]]
+{:user {:plugins [[venantius/ultra "0.5.2"
+                   :exclusions [org.clojure/clojure]]
+                  [lein-cljfmt "0.5.7"
+                   :exclusions [org.clojure/clojure]]
+                  [jonase/eastwood "0.2.5"
+                   :exclusions [org.clojure/clojure]]]
         :repl-options {:init (require 'cljfmt.core)}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [lein-cljfmt "0.5.6"]
-                       [jonase/eastwood "0.2.3"
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"
+                        :exclusions [org.clojure/clojure]]
+                       [lein-cljfmt "0.5.7"
+                        :exclusions [org.clojure/clojure]]
+                       [jonase/eastwood "0.2.5"
+                        :exclusions [org.clojure/clojure]]
+                       [criterium "0.4.4"
                         :exclusions [org.clojure/clojure]]]}}
